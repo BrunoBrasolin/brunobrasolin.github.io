054895764fbcad28fd0857fdfc9fd1e244fe4f37
Bruno Brasolin <brunobrasolinc@gmail.com>
2020-05-21 07:30:43 -0300

    gh pages update

---
+++ build/precache-manifest.ff9c40e5151a3270ad5f70f84d14c670.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95841c7cb154d66c3a8976d1d8f81076",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b27a60cb4bdd31177bd3",
+    "url": "/static/css/main.e1a8d604.chunk.css"
+  },
+  {
+    "revision": "bfcee1891de7375a0fed",
+    "url": "/static/js/2.fc1c90e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc1c90e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b27a60cb4bdd31177bd3",
+    "url": "/static/js/main.2fd57694.chunk.js"
+  },
+  {
+    "revision": "df80ffa8f5c6bbf2e625",
+    "url": "/static/js/runtime-main.109a3340.js"
+  }
+]);
